408c1de3b6337c098e94cc8ac9d8cd60d58c2158
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2026-09-08 12:31:16 +0100

    lab 4.4 completed

--- Starters/Java/4.2 - Test coverage/src/test/java/com/agile/calculator/CalculatorTest.java
+++ Starters/Java/4.2 - Test coverage/src/test/java/com/agile/calculator/CalculatorTest.java
@@ -11,6 +11,73 @@ public class CalculatorTest {
 
 	private Calculator calculator;
 
+	//new tests
+	@Test
+	public void subtract_nonNumberInput_ReturnsMinus9999() {
+		calculator = new Calculator();
+		int expected = -9999;
+		int actual = calculator.subtract("a,1");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void subtract_doubleNonNumberInput_ReturnsMinus9999() {
+		calculator = new Calculator();
+		int expected = -9999;
+		int actual = calculator.subtract("a,b");
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void subtract_inputWithoutComma_ThrowsArrayException() {
+		calculator = new Calculator();
+		int actual = calculator.subtract("12");
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void subtract_emptyInput_ThrowsNumberFormatException() {
+		calculator = new Calculator();
+		int actual = calculator.subtract("");
+	}
+
+	@Test
+	public void divide_doubleNonNumberInput_ReturnsMinus9999() {
+		calculator = new Calculator();
+		int expected = -9999;
+		int actual = calculator.divide("a,b");
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = StringIndexOutOfBoundsException.class)
+	public void divide_inputWithoutComma_ThrowsStringException() {
+		calculator = new Calculator();
+		int actual = calculator.divide("12");
+	}
+
+	@Test(expected = StringIndexOutOfBoundsException.class)
+	public void divide_emptyInput_ThrowsStringException() {
+		calculator = new Calculator();
+		int actual = calculator.divide("");
+	}
+
+	@Test
+	public void divide_noCommaWithSpace_ReturnsMinus9999() {
+		calculator = new Calculator();
+		int expected = 30;
+		int actual = calculator.divide("60 2");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void multiply_nonNumberInput_ReturnsMinus9999() {
+		calculator = new Calculator();
+		int expected = -9999;
+		int actual = calculator.multiply("100, x");
+		assertEquals(expected, actual);
+	}
+	
+	
+
 	@Test
 	public void subtractTest() {
 		calculator = new Calculator();
