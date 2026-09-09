db33d83e15012618913cf3a3078cf35751aad471
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2026-09-09 12:02:17 +0100

    completed lab 07

--- Starters/Java/07 - Code smells/src/main/java/agile/badliskov/com/Account.java
+++ Starters/Java/07 - Code smells/src/main/java/agile/badliskov/com/Account.java
@@ -3,51 +3,150 @@ package agile.badliskov.com;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Account {
+public abstract class Account {
 	// We would have getters/setters for these but left out to minimise the code
 	private String firstname, lastname, address;
 	private int accountNumber;
-	private double interest;
-	private double overdraftLimit;
 	private double balance;
-	private String accountType;
-	private ArrayList<Payee> directDebits = new ArrayList<Payee>();
-	private ArrayList<Payee> standingOrders = new ArrayList<Payee>();
-	
-	
-	public void withDraw(double amount) {
-		if(amount > balance && accountType.equals("deposit"))
-			return;
-		balance -= amount;
+
+	public Account() {
 	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+	public abstract void withDraw(double amount);
+
+
 	public void deposit(double amount) {
 		balance += amount;
 	}	
 	
-	public void addInterestToAccount() { // used where account is a deposit acct
-		balance += balance * 0.0001;
+	
+	
+
+}
+
+class CurrentAccount extends Account {
+
+	private double overdraftLimit;
+	private OrderManagement orderManagement;
+
+
+	CurrentAccount(double overdraftLimit, OrderManagement orderManagement) {
+		super();
+		this.overdraftLimit = overdraftLimit;
+		this.orderManagement = orderManagement;
 	}
+
 	// used where account is current account
 	public boolean isOverDrawn() {
-		return false;
+		return getBalance() < 0;
 	}
-	// used where account is current account
+
+	@Override 
+	public void withDraw(double amount) {
+		if (amount > getBalance() + overdraftLimit) {
+			return;
+		}
+		setBalance(getBalance() - amount);
+	}
+
+		// used where account is current account
 	public void setOverdraftLimit(double overdraftLimit) {
 		this.overdraftLimit = overdraftLimit;
 	}
-	// used where account is current account
-	public void processDirectDebits() {
-		for(Payee payee : directDebits) {
-		}
+
+	public void processPayment(OrderType orderType) {
+		orderManagement.processOrders(orderType);
 	}
-	// used where account is current account
-	public void processStandingOrders() {
-		for(Payee payee : standingOrders) {
-		}
-	}
+	
 }
+
+class DepositAccount extends Account {
+
+	private double interest;
+
+	DepositAccount(double interest) {
+		super();
+		this.interest = interest;
+	}
+
+	public void addInterestToAccount() { // used where account is a deposit acct
+		setBalance(getBalance() + (getBalance() * interest));
+	}
+
+	@Override
+	public void withDraw(double amount) {
+		if (amount > getBalance()) {
+			return;
+		}
+		setBalance(getBalance() - amount);
+	}
+
+}
+
+
+
 class Payee {
 	private String name, address;
 	private Date date;
 	private double amount;
 }
+
+
+
+
+class OrderManagement {
+
+	ArrayList<Order> accountOrders;
+
+	OrderManagement() {
+		accountOrders = new ArrayList<>();
+	}
+
+	public void addOrder(Payee payee, OrderType orderType) {
+		Order newOrder = new Order(payee, orderType);
+		accountOrders.add(newOrder);
+	}
+
+	public void processOrders(OrderType orderType) {
+		for(Order order : accountOrders) {
+			if (order.getOrderType() == orderType){
+				//process
+			}
+		}
+	}
+}
+
+enum OrderType {
+		DIRECT_DEBIT,
+		STANDING_ORDER
+	}
+
+
+class Order {
+
+	private Payee payee;
+	private OrderType orderType;
+
+	Order(Payee payee, OrderType orderType) {
+		this.payee = payee;
+		this.orderType = orderType;
+	}
+
+	public Payee getPayee() {
+		return payee;
+	}
+
+	public OrderType getOrderType() {
+		return orderType;
+	}
+}
+
